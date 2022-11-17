27fbc2197fbe96e9cc6c00ede56bc17b4aa0dceb
Andre Reynaldi Lusikooy <reynaldi@outlook.jp>
2022-11-17 21:42:01 +0700

    update base script

--- prospace.js
+++ prospace.js
@@ -1,13 +1,14 @@
 require('chromedriver')
 let swd = require('selenium-webdriver');
-const { INTERNAL_COMPUTE_OFFSET_SCRIPT } = require('selenium-webdriver/lib/input');
+const { Timeouts } = require('selenium-webdriver/lib/capabilities');
 let driver = new swd.Builder()
     .forBrowser('chrome').build()
+let test = require('selenium-webdriver/testing');
+const timeOut = driver.manage().setTimeouts({
+    implicit: 1000, //ms
+})
+const { site, textField, button } = require('./objects.json')
 
-    const timeOut = driver.manage().setTimeouts({
-        implicit:1000, //ms
-    })
-const { site, textField, button, result } = require('./objects.json')
 let openSite =
     driver.get(site);
 openSite
@@ -15,23 +16,22 @@ let maxWindow = driver.manage().window().maximize()
 maxWindow
 timeOut
 
-    .then(function () {
-        for (let i = 1+1; i < 10; i += 1) {
+    .then(function() {
+        for (let i = 1 + 1; i < 10; i += 1) {
             const timeOut = driver.manage().setTimeouts({
-                implicit:2000, //ms
+                implicit: 2000, //ms
             })
             const sendInt = driver.findElement(swd.By.xpath(textField))
-            let assertInt = sendInt.sendKeys(i)
-            let clickButton = driver.findElement(swd.By.xpath(button)).click()
-            let delItem = driver.executeAsyncScript("document.getElementById('number').value = '';")
-            let getText = driver.findElement({
-                css: 'input[name="number"]'
-              }).getAttribute("value");
-              console.log(getText)
-            return assertInt, clickButton, delItem, timeOut
+            sendInt.sendKeys(i)
+            driver.findElement(swd.By.xpath(button)).click()
+            timeOut
+            sendInt.clear()
+
         }
+    }, 3000)
+    .catch(function(err) {
+        console.log("Error ", err, " occurred!");
+        console.log(`error at ${driver.getCurrentUrl}`);
+        driver.takeScreenshot()
+        driver.close()
     })
-   .catch(function(err) {
-    console.log("Error ", err, " occurred!");
-    driver.close()
-   })
